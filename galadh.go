37c68ace855655c2153b6ed1d103a737cdd19ebb
snmed <sandro@dallo.biz>
2020-08-13 12:09:55 +0200

    Implement getFiles

--- galadh.go
+++ galadh.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -17,6 +18,95 @@ var (
 	asciiGlyphSet   = glyphSet{pipe: "|", last: "`--", item: "|--"}
 	unicodeGlyphSet = glyphSet{pipe: "│", last: "└──", item: "├──"}
 )
+
+// Galadh is simple clone of the posix tree command.
+type Galadh struct {
+	// Listing options
+	allFiles       bool
+	dirOnly        bool
+	excludePattern string
+	ignoreCase     bool
+	includePattern string
+	level          int
+	matchDirs      bool
+	printFullPath  bool
+	// File optionsShowTree
+	humanReadable       bool
+	printSize           bool
+	replaceNonPrintable bool
+	outputFile          bool
+	// Graphic options
+	useASCII  bool
+	useColors bool
+	// Reporting
+	cntFiles int
+	cntDirs  int
+
+	printer treePrinter
+	w       io.Writer
+}
+
+// PrintTree prints a tree according the configured options for
+// the passed directory.
+func (g Galadh) PrintTree(path string) error {
+	fi, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("Galadh: path '%v' does not exist", path)
+		}
+
+		if os.IsPermission(err) {
+			return fmt.Errorf("Galadh: access denied for path '%v'", path)
+		}
+
+		return fmt.Errorf("Galadh: unknown error occured: '%w'", err)
+	}
+
+	if !fi.IsDir() {
+		return fmt.Errorf("Galadh: '%v' is not a directory", path)
+	}
+
+	g.cntDirs, g.cntFiles = 0, 0
+
+	fmt.Fprintln(g.w, path)
+	g.printDir(path, g.level)
+	return nil
+}
+
+func (g *Galadh) printDir(path string, lvl int) {
+
+	_ := g.getFiles(fi)
+}
+
+func (g Galadh) getFiles(path string) (files []os.FileInfo, err error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	fi, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if !fi.IsDir() {
+		return []os.FileInfo{}, nil
+	}
+
+	fs, err := file.Readdir(0)
+	if err != nil {
+		return nil, err
+	}
+
+	// Todo: implement
+	for _, f := range fs {
+		fullPath := filepath.Join(path, f.Name())
+		if !g.allFiles && isHidden()
+
+	}
+
+	return files, nil
+}
 
 type treePrinter struct {
 	glyphs   glyphSet
