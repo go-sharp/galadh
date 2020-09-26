75a6609b8fdb6ae6fc0733b6885626395e080b4e
snmed <sandro@dallo.biz>
2020-09-26 15:40:52 +0200

    Update build script

--- build.sh
+++ build.sh
@@ -3,14 +3,12 @@
 GIT_COMMIT=$(git rev-parse HEAD)
 GIT_TAG=$(git describe --tags --exact-match $GIT_COMMIT)
 
+mkdir -p bin
 echo "Building linux x64 binary..."
-mkdir -p bin/linux/x64
-GOOS=linux GOARCH=amd64 go build -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.gitTag=$GIT_TAG" -o bin/linux/x64/galadh cmd/main.go
+GOOS=linux GOARCH=amd64 go build -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.gitTag=$GIT_TAG" -o bin/galadh_linux_x64 cmd/main.go
 
 echo "Building windows x64 binary..."
-mkdir -p bin/windows/x64
-GOOS=windows GOARCH=amd64 go build -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.gitTag=$GIT_TAG" -o bin/windows/x64/galadh.exe cmd/main.go
+GOOS=windows GOARCH=amd64 go build -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.gitTag=$GIT_TAG" -o bin/galadh_win_x64.exe cmd/main.go
 
 echo "Building macos x64 binary..."
-mkdir -p bin/macos/x64
-GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.gitTag=$GIT_TAG" -o bin/macos/x64/galadh cmd/main.go
+GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.gitCommit=$GIT_COMMIT -X main.gitTag=$GIT_TAG" -o bin/galadh_macos_x64 cmd/main.go
